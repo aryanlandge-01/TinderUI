fc5b5709da46cef93de052309c29f7253bdccad5
aryanlandge-01 <landgearyan0@gmail.com>
2024-12-12 14:51:26 +0530

    Tailwind configuration.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 
 
 function App() {
-  return <h1>Hello Tinder.</h1>
+  return <h1 className="text-3xl text-center font-bold underline">Hello Tinder.</h1>
 }
 
 export default App
---
+++ zung.md
@@ -0,0 +1,5 @@
+# Tinder
+
+- Create a vite + React application
+- Create and lanuch an tinder app.
+
