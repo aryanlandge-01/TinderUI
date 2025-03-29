af1c3751abb31f57dbd69d508cb110737d1030c2
aryanlandge-01 <landgearyan0@gmail.com>
2025-03-29 15:47:58 +0530

    Add socket.io integration for real-time chat functionality

--- src/Components/Chat.jsx
+++ src/Components/Chat.jsx
@@ -1,12 +1,56 @@
-import React from 'react'
-import { useParams } from 'react-router-dom'
+import React, { useState ,useEffect} from 'react'
+import { useParams } from 'react-router-dom';
+import {createSocketConnection} from '../utils/socket';
+import { useSelector } from 'react-redux';
 
 const Chat = () => {
   const {targetUserId} = useParams();
+  const user = useSelector((state) => state.user);
+  const userId = user?._id;
+  const firstName = user?.firstName;
+  if (!userId) {
+    return <div>Please log in to view this page. </div>
+  }
+
+  const [messages,setMessages] = useState([]);
+  const [newMessage, setNewMessage] = useState('');
+
+  useEffect(() => {
+    const socket = createSocketConnection();
+
+    // As soon as the page loads, we join the chat sokcet is created and joinchat event is emitted.
+    socket.emit("joinChat",{userId,targetUserId});
+
+    // listen for incoming messages
+    socket.on("receiveMessage",({firstName,text}) => {
+      console.log(firstName + ": " + text)
+      setMessages((messages) => [...messages,{firstName,text}]);
+    })
+  
+
+    // keep the socket disconnected when the component unmounts.
+    return () => {
+      socket.disconnect();
+    };
+  },[targetUserId])
+
+
+  
+
+  const sendMessage = () => {
+    const socket = createSocketConnection();
+
+    socket.emit("sendMessage",{
+      firstName,
+      userId,
+      targetUserId,
+      text: newMessage
+    });
+  }
 
   return (
     <div className="container mx-auto p-10 mb-10">
-    <h1 className="text-center rounded-md p-4 bg-slate-600 text-white font-semibold">Messages</h1>
+    <h1 className="text-center rounded-md p-4 bg-slate-600 text-white font-semibold">Messages of  {user.firstName}</h1>
 
     <div className="flex flex-col h-[60vh] border rounded-lg overflow-y-auto mb-4">
     {/* Message Area */}
@@ -21,7 +65,9 @@ const Chat = () => {
       {/* User 2 Message */}
       <div className="flex justify-end">
         <div className="bg-gray-200 rounded-lg p-2 max-w-[70%]">
-          <p className="text-sm font-medium text-gray-700">I'm doing great, thanks! What about you?</p>
+          {messages.map((msg,index) => {
+            return  <p key={index} className="text-sm font-medium text-gray-700">{msg.text}</p>
+          })}
         </div>
       </div>
 
@@ -39,11 +85,13 @@ const Chat = () => {
   {/* Input Area */}
     <div className="flex">
     <input
+      value={newMessage}
+      onChange={(e) => setNewMessage(e.target.value)}
       type="text"
       placeholder="Type your message..."
       className="flex-grow border rounded-l-lg p-2 focus:outline-none"
     />
-    <button className="bg-blue-500 text-white rounded-r-lg p-2 px-4 font-semibold">
+    <button onClick={sendMessage} className="bg-blue-500 text-white rounded-r-lg p-2 px-4 font-semibold">
       Send
     </button>
     </div>
